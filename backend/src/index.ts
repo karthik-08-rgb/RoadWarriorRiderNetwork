5cf920b6bd6efac4fde14d008c18f1ad713fd64c
Karthik <Karthik Gotur>
2026-06-03 02:38:56 +0530

    Fix CORS configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || '*'
+    origin: [
+      process.env.FRONTEND_URL || "",
+      "http://localhost:5173"
+    ],
+    credentials: true
   })
 );
 
