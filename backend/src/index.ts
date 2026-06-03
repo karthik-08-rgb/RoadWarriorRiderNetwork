25efe266452f8fb5c82f57e6242bdaff04f522c2
Karthik <Karthik Gotur>
2026-06-03 15:19:21 +0530

    Fix CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,15 +9,16 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      process.env.FRONTEND_URL || "",
-      "http://localhost:5173"
-    ],
-    credentials: true
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       process.env.FRONTEND_URL || "",
+//       "http://localhost:5173"
+//     ],
+//     credentials: true
+//   })
+// );
+app.use(cors());
 
 app.use(express.json());
 
